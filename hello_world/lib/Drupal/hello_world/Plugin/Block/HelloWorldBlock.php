3acd17a4905d6a9936ae9c112f04003658dab06a
Barnett, James <jbarnett@babson.edu>
2013-09-11 14:20:15 -0400

    added a Hello World Basic Block Example

---
+++ hello_world/lib/Drupal/hello_world/Plugin/Block/HelloWorldBlock.php
@@ -0,0 +1,34 @@
+<?php
+ 
+/**
+ * @file
+ * Contains \Drupal\hello_world\Plugin\Block\HelloWorldBlock
+ */
+ 
+namespace Drupal\hello_world\Plugin\Block;
+ 
+use Drupal\block\BlockBase;
+use Drupal\block\Annotation\Block;
+use Drupal\Core\Annotation\Translation;
+
+/**
+ * Provides a simple block.
+ *
+ * @Block(
+ *   id = "hello_world_block",
+ *   admin_label = @Translation("Hello World Block"),
+ *   module = "hello_world"
+ * )
+ */
+class HelloWorldBlock extends BlockBase {
+ 
+  /**
+   * Implements \Drupal\block\BlockBase::blockBuild().
+   */
+  public function build() {
+    $this->configuration['label'] = t('Hello World Block');
+    return array(
+      '#children' => t('Here is the content'),
+    );
+  }
+}
