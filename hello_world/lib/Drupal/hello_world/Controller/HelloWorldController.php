3e6fdcd4050a0c6543186d625b561cbfba8463a6
Barnett, James <jbarnett@babson.edu>
2013-09-30 10:39:56 -0400

    put the html in a variable

--- hello_world/lib/Drupal/hello_world/Controller/HelloWorldController.php
+++ hello_world/lib/Drupal/hello_world/Controller/HelloWorldController.php
@@ -14,6 +14,7 @@ class HelloWorldController extends ContainerAware {
    * Page Callback Method to Print out Hello World Text to the Screen.
    */
   public function myCallbackMethod() {
-    return '<p><b>Saying Hello World in Drupal 8 is cool!</b></p>';
+    $content = '<p><b>Saying Hello World in Drupal 8 is cool!</b></p>';
+    return $content;  
   }
 }
