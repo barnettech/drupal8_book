0c11b37313c14d7c58f764ecb0fe8023ece942a3
Barnett, James <jbarnett@babson.edu>
2013-10-29 16:36:43 -0400

    html lists showcased in our simple Drupal 8 module

--- hello_world/lib/Drupal/hello_world/Controller/HelloWorldController.php
+++ hello_world/lib/Drupal/hello_world/Controller/HelloWorldController.php
@@ -15,20 +15,18 @@ class HelloWorldController extends ContainerAware {
    */
   public function myCallbackMethod() {
     $content = '
-<table border=”1”>
-  <tr>
-    <th>Header 1</th>
-    <th>Header 2</th>
-  </tr>
-  <tr>
-    <td>row 1, cell 1</td>
-    <td>row 1, cell 2</td>
-  </tr>
-  <tr>
-    <td>row 2, cell 1</td>
-    <td>row 2, cell 2</td>
-  </tr>
-</table>
+ORDERED LIST: 
+
+<ol>
+  <li>Item 1</li>
+  <li>Item 2</li>
+</ol>
+
+UNORDERED LIST:
+<ul>
+  <li>Item 1</li>
+  <li>Item 2</li>
+</ul>
       ';
     return $content;  
   }
