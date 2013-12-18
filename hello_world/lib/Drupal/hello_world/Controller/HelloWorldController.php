064c49fda5d3dd32ce07ad4f3c9aa28b5e4daafa
Barnett, James <jbarnett@babson.edu>
2013-12-18 07:58:47 -0500

    hello world with a theme function.

--- hello_world/lib/Drupal/hello_world/Controller/HelloWorldController.php
+++ hello_world/lib/Drupal/hello_world/Controller/HelloWorldController.php
@@ -14,11 +14,9 @@ class HelloWorldController extends ContainerAware {
    * Page Callback Method to Print out Hello World Text to the Screen.
    */
   public function myCallbackMethod() {
-    $content = '<p><b>Here are all the messages recorded in the hello_world custom table! </b></p>';
-    $results = db_query('SELECT * from hello_world');
-    foreach($results as $row) {
-      $content .= 'Row: ' . $row->hid . ', Message: ' . $row->message . ', Timestamp: ' . $row->timestamp . '</br>';
-    }
-    return $content;  
+    return array(
+      '#theme' => 'hello_world_primary_page',
+      '#items' => 1,
+    );
   }
 }
