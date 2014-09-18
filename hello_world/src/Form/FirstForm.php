0b1a1e1ab78621256fafbfbc6111fd0f6105c6db
Barnett, James <jbarnett@babson.edu>
2014-09-18 15:45:03 -0400

    fixed up version of this page using a theme template up to date with the latest drupal 8 now.

--- hello_world/src/Form/FirstForm.php
+++ hello_world/src/Form/FirstForm.php
@@ -7,20 +7,13 @@
  
 namespace Drupal\hello_world\Form;
 
-use Drupal\Core\Form\FormInterface;
-use Drupal\Core\Form\FormBuilder;
-use Drupal\Core\Session\AccountInterface;
-
-use Drupal\Core\Entity\EntityInterface;
-use Drupal\entity\Entity\EntityDisplay;
-use Drupal\node\NodeInterface;
-use Drupal\rest\Tests\RESTTestBase;
-
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
  
 /**
  * Provides a simple example form.
  */
-class FirstForm implements FormInterface {
+class FirstForm extends FormBase {
  
   /**
    * Implements \Drupal\Core\Form\FormInterface::getFormID().
@@ -32,7 +25,7 @@ class FirstForm implements FormInterface {
   /**
    * Implements \Drupal\Core\Form\FormInterface::buildForm().
    */
-  public function buildForm(array $form, array &$form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state) {
     // Use the Form API to define form elements.
     $form['message'] = array(
       '#title' => t('Enter your message to insert into the hello_world table'),
@@ -48,14 +41,14 @@ class FirstForm implements FormInterface {
   /**
    * Implements \Drupal\Core\Form\FormInterface::validateForm().
    */
-  public function validateForm(array &$form, array &$form_state) {
+  public function validateForm(array &$form, FormStateInterface $form_state) {
     // Validate the form values.
   }
  
   /**
    * Implements \Drupal\Core\Form\FormInterface::submitForm().
    */
-  public function submitForm(array &$form, array &$form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state) {
     $account = \Drupal::currentUser();
     db_query("INSERT INTO hello_world (uid, message, timestamp) values(:uid, :message, :timestamp)", array(':uid' => $account->id(), ':message' => $form_state['values']['message'], ':timestamp' => time()));
     drupal_set_message('Your form was submitted successfully, you typed in the body ' . $form_state['values']['message']);
